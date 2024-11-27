4d3506fbe5b3857769a753a2401b5370b44e50e3
alicemariaarrica <alicemariaarrica@gmail.com>
2024-11-27 12:40:57 +0100

    PostContext.jsx-upgrade

---
+++ src/Context/PostContext.jsx
@@ -0,0 +1,19 @@
+import React, { createContext, useState, useContext } from 'react';
+import exoticFruits from './PostsLists';
+
+
+const FruitContext = createContext();
+
+
+export const useFruits = () => useContext(FruitContext);
+
+
+export const FruitProvider = ({ children }) => {
+    const [fruits, setFruits] = useState(exoticFruits);
+
+    return (
+        <FruitContext.Provider value={{ fruits, setFruits }}>
+            {children}
+        </FruitContext.Provider>
+    );
+};
